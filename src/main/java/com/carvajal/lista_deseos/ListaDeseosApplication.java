b18bd23198afc3a39b0292d576134273605367be
Cristian Velasquez <crisdelmaso@gmail.com>
2026-09-17 19:28:38 -0500

    Inicialización del proyecto

---
+++ src/main/java/com/carvajal/lista_deseos/ListaDeseosApplication.java
@@ -0,0 +1,13 @@
+package com.carvajal.lista_deseos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeseosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeseosApplication.class, args);
+	}
+
+}
